cbf871d2f4eeb9de5b93e1b5c6fbccb56d8e7419
nadiellymedeiros <nadiellymedeiros@gmail.com>
2024-01-24 10:45:00 -0300

    Moved the worksImg folder to assets and edited the order of worksData.

--- src/data/worksData.jsx
+++ src/data/worksData.jsx
@@ -1,72 +1,22 @@
-import analogicClock from "./worksImg/analogicClock.png";
-import calculatorBlackOrWhite from "./worksImg/calculatorBlackOrWhite.png";
-import calculatorWhithHistory from "./worksImg/calculatorWhiteHistory.png";
-import creditCard from "./worksImg/creditCard.png";
-import financialControl from "../assets/financialControl.png";
-import imaFashion from "./worksImg/imaFashion.png";
-import marioGame from "./worksImg/marioGame.png";
-import ticTacToe from "./worksImg/ticTacToe.png";
-import newsAPI from "./worksImg/newsAPI.png";
-import investmentsControl from "./worksImg/investmentsControl.png";
-import portifolio from "./worksImg/portifolio.png";
-import simpleToDo from "./worksImg/simpleToDo.png";
-import taskManager from "./worksImg/taskManager.png";
-import githubFinder from "./worksImg/githubFinder.png";
-import myBanking from "./worksImg/myBanking.png";
+import analogicClock from "../assets/worksImg/analogicClock.png";
+import calculatorBlackOrWhite from "../assets/worksImg/calculatorBlackOrWhite.png";
+import calculatorWhithHistory from "../assets/worksImg/calculatorWhiteHistory.png";
+import creditCard from "../assets/worksImg/creditCard.png";
+import financialControl from "../assets/worksImg/financialControl.png";
+import imaFashion from "../assets/worksImg/imaFashion.png";
+import marioGame from "../assets/worksImg/marioGame.png";
+import ticTacToe from "../assets/worksImg/ticTacToe.png";
+import newsAPI from "../assets/worksImg/newsAPI.png";
+import investmentsControl from "../assets/worksImg/investmentsControl.png";
+import portifolio from "../assets/worksImg/portifolio.png";
+import simpleToDo from "../assets/worksImg/simpleToDo.png";
+import taskManager from "../assets/worksImg/taskManager.png";
+import githubFinder from "../assets/worksImg/githubFinder.png";
+import myBanking from "../assets/worksImg/myBanking.png";
 
 export const worksData = [
   {
     id: "0",
-    img: analogicClock,
-    worksName: "Relógio Analógico",
-    url: "https://timely-zuccutto-97165b.netlify.app/",
-    github: "https://github.com/nadiellymedeiros/analog-clock",
-    Description: "Relógio analógico que mostra a hora atual",
-    stacks: ["Javascript", "HTML", "CSS"],
-  },
-
-  {
-    id: "1",
-    img: calculatorBlackOrWhite,
-    worksName: "Calculadora",
-    url: "https://funny-monstera-e003a7.netlify.app/",
-    github: "https://github.com/nadiellymedeiros/calculator-black-or-white",
-    Description: "Calculadora com tema preto ou branco",
-    stacks: ["Javascript", "HTML", "CSS"],
-  },
-
-  {
-    id: "2",
-    img: calculatorWhithHistory,
-    worksName: "Calculadora com histórico",
-    url: "https://dreamy-syrniki-8a61db.netlify.app/",
-    github: "https://github.com/nadiellymedeiros/Calculator-with-history",
-    Description: "Calculadora simples que mostra o histórico de cálculos",
-    stacks: ["Javascript", "HTML", "CSS"],
-  },
-  {
-    id: "3",
-    img: creditCard,
-    worksName: "Cartão de crédito",
-    url: "https://genuine-melomakarona-366608.netlify.app/",
-    github: "https://github.com/nadiellymedeiros/credit-card",
-    Description:
-      "Cartão de crédito que altera layout de acordo com que preenche",
-    stacks: ["Javascript", "HTML", "CSS", "REGEX"],
-  },
-
-  {
-    id: "4",
-    img: financialControl,
-    worksName: "Controle Financeiro",
-    url: "https://nimble-nasturtium-aeb258.netlify.app/",
-    github: "https://github.com/nadiellymedeiros/Financial-Control",
-    Description: "Controle financeiro com entrada e saída",
-    stacks: ["Javascript", "HTML", "CSS, React"],
-  },
-
-  {
-    id: "5",
     img: imaFashion,
     worksName: "Imã Fashion",
     url: "https://glistening-manatee-92b9a4.netlify.app/",
@@ -77,27 +27,18 @@ export const worksData = [
   },
 
   {
-    id: "6",
-    img: marioGame,
-    worksName: "Jogo do Mário",
-    url: "https://earnest-crostata-12dcfd.netlify.app/",
-    github: "https://github.com/nadiellymedeiros/mario-game",
-    Description: "Jogo simples do Mario",
-    stacks: ["Javascript", "HTML", "CSS"],
+    id: "1",
+    img: myBanking,
+    worksName: "Meu Banco",
+    url: "https://github.com/nadiellymedeiros/my-bank-frontend",
+    github: "https://github.com/nadiellymedeiros/my-bank-frontend",
+    Description:
+      "Banco feito em Angular, o usuário pode logar e fazer as transações de saque depósito e transferência",
+    stacks: ["HTML", "CSS", "Javascript, Angular"],
   },
 
   {
-    id: "7",
-    img: ticTacToe,
-    worksName: "Jogo da velha",
-    url: "https://chipper-cocada-c7bf24.netlify.app/",
-    github: "https://github.com/nadiellymedeiros/tic-tac-toe_Jogo-da-velha",
-    Description: "Jogo da velha",
-    stacks: ["Javascript", "HTML", "CSS"],
-  },
-
-  {
-    id: "8",
+    id: "2",
     img: newsAPI,
     worksName: "Tech pro bem",
     url: "https://challenge-news-tech-probem.vercel.app/",
@@ -107,17 +48,7 @@ export const worksData = [
   },
 
   {
-    id: "9",
-    img: investmentsControl,
-    worksName: "Controle de investimentos",
-    url: "https://lucky-sfogliatella-1d205b.netlify.app/",
-    github: "https://github.com/nadiellymedeiros/investment-control",
-    Description: "Controle de investimento separando por categorias",
-    stacks: ["HTML", "CSS", "Javascript, React"],
-  },
-
-  {
-    id: "10",
+    id: "3",
     img: portifolio,
     worksName: "Portifólio",
     url: "https://rainbow-khapse-aacd1c.netlify.app/",
@@ -128,16 +59,7 @@ export const worksData = [
   },
 
   {
-    id: "11",
-    img: simpleToDo,
-    worksName: "To do simples",
-    url: "https://to-do-simple-leoujo.vercel.app/",
-    github: "https://github.com/nadiellymedeiros/to-do-Simple-Leoujo",
-    Description: "To do list com checkbox , excluir e editar a tarefa",
-    stacks: ["HTML", "CSS", "Javascript"],
-  },
-  {
-    id: "12",
+    id: "4",
     img: taskManager,
     worksName: "Gerenciador de Tarefas",
     url: "https://graceful-pudding-16476e.netlify.app/pages/taskmanager/taskmanager?",
@@ -145,8 +67,19 @@ export const worksData = [
     Description: "Gerenciador de tarefas com status",
     stacks: ["HTML", "CSS", "Javascript"],
   },
+
   {
-    id: "13",
+    id: "5",
+    img: investmentsControl,
+    worksName: "Controle de investimentos",
+    url: "https://lucky-sfogliatella-1d205b.netlify.app/",
+    github: "https://github.com/nadiellymedeiros/investment-control",
+    Description: "Controle de investimento separando por categorias",
+    stacks: ["HTML", "CSS", "Javascript, React"],
+  },
+
+  {
+    id: "6",
     img: githubFinder,
     worksName: "Buscador de usuários no github",
     url: "https://splendid-pithivier-04cb62.netlify.app/",
@@ -154,14 +87,85 @@ export const worksData = [
     Description: "Gerenciador de tarefas com status",
     stacks: ["HTML", "CSS", "Javascript, React"],
   },
+
+  {
+    id: "7",
+    img: simpleToDo,
+    worksName: "To do simples",
+    url: "https://to-do-simple-leoujo.vercel.app/",
+    github: "https://github.com/nadiellymedeiros/to-do-Simple-Leoujo",
+    Description: "To do list com checkbox , excluir e editar a tarefa",
+    stacks: ["HTML", "CSS", "Javascript"],
+  },
+
+  {
+    id: "8",
+    img: analogicClock,
+    worksName: "Relógio Analógico",
+    url: "https://timely-zuccutto-97165b.netlify.app/",
+    github: "https://github.com/nadiellymedeiros/analog-clock",
+    Description: "Relógio analógico que mostra a hora atual",
+    stacks: ["Javascript", "HTML", "CSS"],
+  },
+
+  {
+    id: "9",
+    img: calculatorBlackOrWhite,
+    worksName: "Calculadora",
+    url: "https://funny-monstera-e003a7.netlify.app/",
+    github: "https://github.com/nadiellymedeiros/calculator-black-or-white",
+    Description: "Calculadora com tema preto ou branco",
+    stacks: ["Javascript", "HTML", "CSS"],
+  },
+
+  {
+    id: "10",
+    img: calculatorWhithHistory,
+    worksName: "Calculadora com histórico",
+    url: "https://dreamy-syrniki-8a61db.netlify.app/",
+    github: "https://github.com/nadiellymedeiros/Calculator-with-history",
+    Description: "Calculadora simples que mostra o histórico de cálculos",
+    stacks: ["Javascript", "HTML", "CSS"],
+  },
+
+  {
+    id: "11",
+    img: creditCard,
+    worksName: "Cartão de crédito",
+    url: "https://genuine-melomakarona-366608.netlify.app/",
+    github: "https://github.com/nadiellymedeiros/credit-card",
+    Description:
+      "Cartão de crédito que altera layout de acordo com que preenche",
+    stacks: ["Javascript", "HTML", "CSS", "REGEX"],
+  },
+
+  {
+    id: "12",
+    img: financialControl,
+    worksName: "Controle Financeiro",
+    url: "https://nimble-nasturtium-aeb258.netlify.app/",
+    github: "https://github.com/nadiellymedeiros/Financial-Control",
+    Description: "Controle financeiro com entrada e saída",
+    stacks: ["Javascript", "HTML", "CSS, React"],
+  },
+
+  {
+    id: "13",
+    img: marioGame,
+    worksName: "Jogo do Mário",
+    url: "https://earnest-crostata-12dcfd.netlify.app/",
+    github: "https://github.com/nadiellymedeiros/mario-game",
+    Description: "Jogo simples do Mario",
+    stacks: ["Javascript", "HTML", "CSS"],
+  },
+
   {
     id: "14",
-    img: myBanking,
-    worksName: "Meu Banco",
-    url: "https://github.com/nadiellymedeiros/my-bank-frontend",
-    github: "https://github.com/nadiellymedeiros/my-bank-frontend",
-    Description:
-      "Banco feito em Angular, o usuário pode logar e fazer as transações de saque depósito e transferência",
-    stacks: ["HTML", "CSS", "Javascript, Angular"],
+    img: ticTacToe,
+    worksName: "Jogo da velha",
+    url: "https://chipper-cocada-c7bf24.netlify.app/",
+    github: "https://github.com/nadiellymedeiros/tic-tac-toe_Jogo-da-velha",
+    Description: "Jogo da velha",
+    stacks: ["Javascript", "HTML", "CSS"],
   },
 ];
